7ade9e334313417a659b5c522719d7407ca69137
paupolytropo <pau@polytropo.es>
2021-08-05 01:23:14 +0200

    fix: url

--- apps/authentication/external/http/test/api.spec.ts
+++ apps/authentication/external/http/test/api.spec.ts
@@ -1,9 +1,10 @@
 import request from "supertest";
 
 import app from "../main/app";
+import {baseUrl} from "../main/configuration";
 
 describe("GET /api", () => {
   it("should return 200 OK", () => {
-    return request(app).get("/api").expect(200);
+    return request(app).get(baseUrl).expect(200);
   });
 });
